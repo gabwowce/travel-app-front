b85344cec678331c64a601e15a16cf67721863ff
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2025-05-14 07:41:25 +0300

    Update registerSchema.ts

--- src/validation/registerSchema.ts
+++ src/validation/registerSchema.ts
@@ -1,1 +1,25 @@
 
+import * as Yup from 'yup';
+
+export const registerSchema = Yup.object({
+  name: Yup.string()
+    .trim()
+    .min(2, 'Vardas per trumpas')
+    .max(50, 'Vardas per ilgas')
+    .required('Vardas privalomas'),
+
+  email: Yup.string()
+    .trim()
+    .email('Įvesk galiojantį el. paštą')
+    .required('El. paštas privalomas'),
+
+  password: Yup.string()
+    .min(6, 'Slaptažodį turi sudaryti bent 6 simboliai')
+    .matches(/[A-Z]/, 'Slaptažodyje turi būti bent viena didžioji raidė')
+    .matches(/[0-9]/, 'Slaptažodyje turi būti bent vienas skaičius')
+    .required('Slaptažodis privalomas'),
+
+  password_confirmation: Yup.string()
+    .oneOf([Yup.ref('password')], 'Slaptažodžiai nesutampa')
+    .required('Pakartoti slaptažodį privaloma'),
+});
