bd6aea2ea12c1567803281a576e4927bce6b6fa0
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2025-05-14 07:45:51 +0300

    Update loginSchema.ts

--- src/validation/loginSchema.ts
+++ src/validation/loginSchema.ts
@@ -1,6 +1,6 @@
-// validation/loginSchema.ts
+il// validation/loginSchema.ts
 import * as Yup from 'yup';
 export const loginSchema = Yup.object({
-  email: Yup.string().email('Email is not valid').required('Required'),
-  password: Yup.string().min(6).required('Required'),
+  email: Yup.string().email('Email is not valid').required('Email is Required'),
+  password: Yup.string().min(6).required('Password is Required'),
 });
