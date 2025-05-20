9ea1ca5e350e88ce5328a997a522989c2dc34622
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2025-05-20 13:58:58 +0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,14 @@
 // app/index.tsx
-import { Redirect } from 'expo-router';
-import { useAppSelector } from '@/src/data/hooks';
+//import { Redirect } from 'expo-router';
+//import { useAppSelector } from '@/src/data/hooks';
 
-export default function Index() {
-  const isAuthenticated = useAppSelector(state => state.auth.isAuthenticated);
-  const onboardingDone = true; // arba imk iš globalios būsenos jei reikia
+//export default function Index() {
+ // const isAuthenticated = useAppSelector(state => state.auth.isAuthenticated);
+ // const onboardingDone = true; // arba imk iš globalios būsenos jei reikia
 
-  if (!isAuthenticated) {
-    return <Redirect href="/(auth)" />;
-  }
+ // if (!isAuthenticated) {
+ //   return <Redirect href="/(auth)" />;
+ // }
 
-  return <Redirect href="/(app)/(tabs)/home" />;
-}
+//  return <Redirect href="/(app)/(tabs)/home" />;
+//}
