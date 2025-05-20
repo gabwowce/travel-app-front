c25585030eb158d31329f9496c5da2208efad185
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2025-05-20 14:46:47 +0300

    Create StatChip.tsx

---
+++ src/components/ui/StatChip.tsx
@@ -0,0 +1,28 @@
+// components/ui/StatChip.tsx
+import React from "react";
+import { HStack, Icon, Text } from "native-base";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function StatChip({
+  icon,
+  label,
+}: {
+  icon: string;
+  label: string;
+}) {
+  return (
+    <HStack
+      alignItems="center"
+      bg="primary.100"
+      _dark={{ bg: "primary.800" }}
+      px="3"
+      py="1"
+      rounded="md"
+      mr="2"
+      mb="2"
+    >
+      <Icon as={FontAwesome} name={icon} size="xs" mr="1" />
+      <Text fontSize="xs">{label}</Text>
+    </HStack>
+  );
+}
